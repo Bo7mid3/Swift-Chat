3d8da48fd28b9abed528614bf0bc373ca0c4f814
ahmed <51823905+ahmed25261@users.noreply.github.com>
2021-03-25 10:38:18 +0100

    feat<register>:upload user img

--- src/components/register/index.js
+++ src/components/register/index.js
@@ -1,5 +1,24 @@
 import React from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 
-const Register = () => <div></div>
+const Register = () => {
+    //const { register, handleSubmit } = useForm();
+
+    //const onSubmit = ()
+
+    return (
+    <div>
+    <form action="https://SwiftChatServer.ahmedbahloul.repl.co/register" enctype="multipart/form-data" method="post" target="dummyframe">
+        <input type="text" name="username" placeholder="username" />
+        <input type="password" name="password" placeholder="password" />
+        <input type="text" name="email" placeholder="email" />
+        <input type="file" name="user_img" />
+        <input type="submit" />
+    </form>
+    <iframe name="dummyframe" id="dummyframe"></iframe>
+    </div>
+    )
+}
 
 export default Register;
